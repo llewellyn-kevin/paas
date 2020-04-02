3944ba5c1f77f6d47e04f44ca4fa6f2b09e2aa95
Kevin Llewellyn <llewellyn.kevin@yahoo.com>
2020-04-01 21:19:29 -0600

    Added redis connection and library

--- server/main.go
+++ server/main.go
@@ -1,32 +1,47 @@
 package main
 
 import(
+  "fmt"
   "net/http"
 
   "github.com/gin-gonic/gin"
+  "github.com/gomodule/redigo/redis"
+)
+
+var(
+  store redis.Conn
 )
 
 
 func main() {
+  var err error
+  store, err = redis.Dial("tcp", ":6379")
+  if err != nil {
+    panic(err)
+  }
+  defer store.Close()
+
   router := gin.Default()
 
   v1 := router.Group("/v1")
   {
     v1.POST("/users", statusCheck)                      // Create user account, create JWA, send to user
-    v1.POST("/signin", statusCheck)               // Create JWA, send to user
-    v1.POST("/signout", statusCheck)              // Destroy JWA
+    v1.POST("/signin", statusCheck)                     // Create JWA, send to user
+    v1.POST("/signout", statusCheck)                    // Destroy JWA
 
     v1.GET("/users/:uid", statusCheck)                  // Get user account
     v1.PUT("/users/:uid", statusCheck)                  // Update user account
     v1.DELETE("/users/:uid", statusCheck)               // Delete user account
 
     v1.GET("/users/:uid/accounts", statusCheck)         // Get accounts, without passwords
-    v1.POST("/users/accounts", statusCheck)        // Create new account, generate and encrypt password
+    v1.POST("/users/accounts", statusCheck)             // Create new account, generate and encrypt password
 
     v1.GET("/users/:uid/accounts/:aid", statusCheck)    // Get account, including password
     v1.PUT("/users/:uid/accounts/:aid", statusCheck)    // Update account
     v1.DELETE("/users/:uid/accounts/:aid", statusCheck) // Delete account
   }
+
+  fmt.Println(redis.String(store.Do("Get", "myname")))
 
   router.Run(":6001")
 }
